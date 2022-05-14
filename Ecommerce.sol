95c1d5a546b839acef7aeec99a462116a7b77c24
"sachincoder1" <"sachinhlo232@gmail.com">
2022-05-14 16:02:10 +0530

    product struct

--- Ecommerce.sol
+++ Ecommerce.sol
@@ -1,3 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
+contract Ecommerce {
+
+    //  creating struct for product
+    struct Product = {
+        string title;
+        string description;
+        address payable  seller;
+        uint price;
+        address buyer;
+        bool delivered
+    }
+}
